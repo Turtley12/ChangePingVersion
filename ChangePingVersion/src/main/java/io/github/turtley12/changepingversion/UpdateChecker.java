b388a55a0f9085c6131f023321d8ad122c4bdf4d
Turtley12 <76929530+Turtley12@users.noreply.github.com>
2021-01-10 09:52:54 -0800

    Fix Indentation

--- ChangePingVersion/src/main/java/io/github/turtley12/changepingversion/UpdateChecker.java
+++ ChangePingVersion/src/main/java/io/github/turtley12/changepingversion/UpdateChecker.java
@@ -14,48 +14,48 @@ public class UpdateChecker {
 	private static String new_version;
 	public static void startUpdateChecker() {
 		Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
-				public void run() {
-					try {
-						Bukkit.getLogger().info("ChangePingVersion checking for updates...");
-					//check githubs api for latest version
-					URL url = new URL("https://api.github.com/repos/turtley12/changepingversion/releases");
+			public void run() {
+				try {
+					Bukkit.getLogger().info("ChangePingVersion checking for updates...");
+				//check githubs api for latest version
+				URL url = new URL("https://api.github.com/repos/turtley12/changepingversion/releases");
+			
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.connect();
 				
-					HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-					conn.setRequestMethod("GET");
-					conn.connect();
-					
-					//Getting the response code
-					int responsecode = conn.getResponseCode();
-					
-					if (responsecode == 200) {
-						String inline = "";
-					    Scanner scanner = new Scanner(url.openStream());
-					  
-					   //Write all the JSON data into a string using a scanner
-					    while (scanner.hasNext()) {
-					       inline += scanner.nextLine();
-					    }
-					    
-					    //Close the scanner
-					    scanner.close();
-					    
-					    JsonObject[] jobj = new Gson().fromJson(inline, JsonObject[].class);
-					    
+				//Getting the response code
+				int responsecode = conn.getResponseCode();
 				
-					    new_version = jobj[0].get("tag_name").toString();
-					    
-					    if (UpdateChecker.getUpdateAvailable()) {
-							Bukkit.getLogger().info("ChangePingVersion Update " + UpdateChecker.getNewVersion().replace("\"", "") + " is available");
-							Bukkit.getLogger().info("You have version " + Main.getVersion());
-							Bukkit.getLogger().info("Download the new release at: ");
-							Bukkit.getLogger().info("https://github.com/Turtley12/ChangePingVersion/releases");
-					    }
-				
-					}
-					else {
-						Bukkit.getLogger().info("Error checking for updates");
-						Bukkit.getLogger().info("response code = " + responsecode);
-					}
+				if (responsecode == 200) {
+					String inline = "";
+				    Scanner scanner = new Scanner(url.openStream());
+				  
+				   //Write all the JSON data into a string using a scanner
+				    while (scanner.hasNext()) {
+				       inline += scanner.nextLine();
+				    }
+				    
+				    //Close the scanner
+				    scanner.close();
+				    
+				    JsonObject[] jobj = new Gson().fromJson(inline, JsonObject[].class);
+				    
+			
+				    new_version = jobj[0].get("tag_name").toString();
+				    
+				    if (UpdateChecker.getUpdateAvailable()) {
+						Bukkit.getLogger().info("ChangePingVersion Update " + UpdateChecker.getNewVersion().replace("\"", "") + " is available");
+						Bukkit.getLogger().info("You have version " + Main.getVersion());
+						Bukkit.getLogger().info("Download the new release at: ");
+						Bukkit.getLogger().info("https://github.com/Turtley12/ChangePingVersion/releases");
+				    }
+			
+				}
+				else {
+					Bukkit.getLogger().info("Error checking for updates");
+					Bukkit.getLogger().info("response code = " + responsecode);
+				}
 					
 				} catch (Exception e) {
 					//if something went wrong do this
